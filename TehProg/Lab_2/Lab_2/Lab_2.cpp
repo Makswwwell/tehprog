617e36cf123aef11c8949de51e17ff869a1ebf98
MaKsiMkA __ <makswwwell@gmail.com>
2023-10-20 10:39:06 +0300

    Lab_2_done

--- TehProg/Lab_2/Lab_2/Lab_2.cpp
+++ TehProg/Lab_2/Lab_2/Lab_2.cpp
@@ -17,7 +17,7 @@ string* reverse_arr(string* arr, int n) {
 }
 
 
-string task_using_string(string str) {
+string revers_string(string str) {
     int index = -1;
     int n = 0;
     string* str_arr = nullptr;
@@ -70,25 +70,55 @@ string task_using_string(string str) {
     }
 }
 
-/*
-const char** reverse_arr_char(const char** arr, int n) {
-    for (int i = 0; i < n / 2; i++) {
-        const char* temp = arr[i];
-        arr[i] = arr[n - 1 - i];
-        arr[n - 1 - i] = temp;
+
+// __________________________________________________________________________
+
+// Работа дял cstring
+char* reverse(const char* str) {
+    int length = strlen(str);
+    char* reversedStr = new char[length + 1]; // +1 для нулевого символа
+
+    int wordEnd = length - 1;  // Индекс конца текущего слова
+    int reversedIndex = 0;    // Индекс, куда будем записывать обратное слово
+
+    // Обходим строку справа налево
+    for (int i = length - 1; i >= 0; i--) {
+        if (str[i] == ' ' || i == 0) {
+            // Если найден разделитель или достигнут конец строки, скопируем слово в обратном порядке
+            int wordStart = (i == 0) ? 0 : i + 1;
+
+            for (int j = wordStart; j <= wordEnd; j++) {
+                reversedStr[reversedIndex++] = str[j];
+            }
+
+            if (i > 0) {
+                // Если это не конец строки, добавим пробел после слова
+                reversedStr[reversedIndex++] = ' ';
+            }
+
+            // Установим индекс конца текущего слова
+            wordEnd = i - 1;
+        }
     }
 
-    return arr;
+    reversedStr[reversedIndex] = '\0'; // Завершаем строку нулевым символом
+
+    return reversedStr;
 }
 
-const char* task_using_cstring(const char* str) {
-    int index = -1;
-    int n = 0;
-    const char** str_arr = nullptr;
+// Освобождение памяти для перевернутой строки
+void free_cstring(const char* str) {
+    delete[] str;
+}
 
-    if (strlen(str) % 2 == 1) {
-        for (int i = 0; i < strlen(str); i++) {
-            if (isdigit(str[i])) {
+const char* revers_cstring(const char* input) {
+    int index = -1;
+    if (strlen(input) % 2 == 1)
+    {
+        for (size_t i = 0; i < strlen(input); i++)
+        {
+            if (isdigit(input[i]))
+            {
                 index = i;
                 break;
             }
@@ -98,54 +128,40 @@ const char* task_using_cstring(const char* str) {
             return "В строке нет чисел";
         }
 
-        // Выделяем память для массива указателей на строки
-        n = strlen(str) - index;
-        str_arr = new const char* [n];
+        const char* substr = input + index;
 
-        const char* words = "";
-        int j = 0;
-
-        for (size_t i = index; i < strlen(str); i++) {
-            words += str[i];
-            if (str[i] == ' ' || i == strlen(str) - 1) {
-                str_arr[j++] = words;
-                words = "";
-            }
-        }
-
-        // Переворачиваем массив указателей на строки
-        reverse_arr_char(str_arr, n);
-
-        const char* result = "";
-
-        for (int i = 0; i < n; i++) {
-            if (str_arr[i][0] != '\0') {
-                result += str_arr[i];
-                result += " ";
-            }
-        }
-
-        // Освобождаем память, выделенную для str_arr
-        delete[] str_arr;
-
-        return result;
+        return reverse(substr);
     }
+
     else {
         return "Строка имеет четное количество символов";
     }
 }
-*/
+
 int main()
 {
 	setlocale(LC_ALL, "Russian");
 
-	string a = "qwer w1yt ei"; // четное
+	string a = "Мама купила 33 апельсина"; // четное
 	string b = "Мама купила 3 апельсина";  // нечетное
+    string c = "Мама не купила апельсинов";  // нет чисел 
 
-	cout << task_using_string(b) << endl;
-	cout << task_using_string(a) << endl;
+    cout << "Реализация дял string" << endl;
+    cout << revers_string(a) << endl;
+	cout << revers_string(b) << endl;
+    cout << revers_string(c) << endl;
 	cout << endl;
 
+    const char* ca = "Мама купила 33 апельсина"; // четное
+    const char* cb = "Мама купила 3 апельсина";  // нечетное
+    const char* cc = "Мама не купила апельсинов";  // нет чисел
+
+
+    cout << "Реализация дял cstring" << endl;
+    cout << revers_cstring(ca) << endl;
+    cout << revers_cstring(cb) << endl;
+    cout << revers_cstring(cc) << endl;
+    cout << endl;
 
 	return 0;
 }
